25dc5e64a416022323d7e1bc3b5824b47c15190d
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2023-01-27 23:19:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -765,7 +765,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
          fmsg = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
        
-    await asyncio.sleep(900)
+    await asyncio.sleep(300)
     await fmsg.delete()
     
     if spoll:
